197435021bd63545b55b5b02d669f7051a108caa
1612543 <quitland2010@gmail.com>
2019-09-24 19:30:22 +0700

    add force

--- JelllyAdventure/Assets/Resource/Character/ControlCharacter.cs
+++ JelllyAdventure/Assets/Resource/Character/ControlCharacter.cs
@@ -7,39 +7,49 @@ public class ControlCharacter : MonoBehaviour
 {
     [SerializeField] private float speed = 0.8f;
     Vector3 pos;
+    Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
         pos = transform.position;
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        pos = transform.position;
         if (PressKeyMove())
         {
             if (PressKeyMoveLeft())
             {
                 GetComponent<Animator>().Play("Run");
                 GetComponent<SpriteRenderer>().flipX = true;
-                if (pos.x >= -8)
-                    pos.x -= speed;
+                //if (pos.x >= -8)
+                {
+                    rigidbody.AddForce(new Vector2(-speed, 0));
+                }
             }
             else if (PressKeyMoveRight())
             {
                 GetComponent<Animator>().Play("Run");
                 GetComponent<SpriteRenderer>().flipX = false;
-                pos.x += speed;
+                rigidbody.AddForce(new Vector2(speed, 0));
             }
-                
         }
         else GetComponent<Animator>().Play("Blink");
-        transform.position = pos;
+        //transform.position = pos;
+    }
+
+    private bool PressKeyMoveUp()
+    {
+        return Input.GetKey(KeyCode.UpArrow);
     }
 
     private bool PressKeyMove()
     {
-        return PressKeyMoveLeft() || PressKeyMoveRight();
+        return PressKeyMoveLeft() || PressKeyMoveRight() || PressKeyMoveUp();
     }
 
     private bool PressKeyMoveRight()
