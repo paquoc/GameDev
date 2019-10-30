674e076a2da1ea33e41bd36ebf3c3875c499912f
AnhQuoc <quitland2010@gmail.com>
2019-10-31 00:53:08 +0700

    add script UserIInfo

---
+++ JelllyAdventure/Assets/Scripts/UserInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfo:MonoBehaviour
+{
+    private int score = 0;
+    private int live = 2;
+    
+    public static UserInfo instance { get; private set;}
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void addScore(int v)
+    {
+        this.score += v;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+}
