7a9e81be94602fea716fb5d8f6fba0304adf1af7
AnhQuoc <quitland2010@gmail.com>
2019-10-31 00:52:45 +0700

    script IngameUI to mange UI

---
+++ JelllyAdventure/Assets/Scripts/IngameUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngameUI : MonoBehaviour
+{
+    public Text labelNumCoin;
+    public static IngameUI instance { get; private set; }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateLabelNumCoin()
+    {
+        int numCoin = UserInfo.instance.getScore();
+        labelNumCoin.text = numCoin.ToString();
+    }
+}
