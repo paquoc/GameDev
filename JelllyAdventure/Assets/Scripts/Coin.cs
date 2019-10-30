95f81aac83a46b50c03bb4d6e242260f6f2d21ca
AnhQuoc <quitland2010@gmail.com>
2019-10-31 00:52:22 +0700

    add script for coin: detect collision and remove coin

---
+++ JelllyAdventure/Assets/Scripts/Coin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private bool exist = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (exist)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                exist = false;
+                UserInfo.instance.addScore(1);
+                IngameUI.instance.UpdateLabelNumCoin();
+                this.removeFromScene();
+            }
+        }
+        
+    }
+
+    private void removeFromScene()
+    {
+        Destroy(this.gameObject);
+    }
+}
