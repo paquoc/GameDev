570293caf7cd7a42b6921d82eacc93bb284d89a6
AnhQuoc <quitland2010@gmail.com>
2019-11-19 17:53:42 +0700

    add Project 3D

--- JelllyAdventure/Assets/Scripts/IngameUI.cs
+++ JelllyAdventure/Assets/Scripts/IngameUI.cs
@@ -11,6 +11,7 @@ public class IngameUI : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        labelNumCoin.text = testGameData.instance.GetScore().ToString();
     }
     // Start is called before the first frame update
     void Start()
---
+++ JelllyAdventure/Assets/Scripts/testGameData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testGameData : MonoBehaviour
+{
+
+    public static testGameData instance;
+    // Start is called before the first frame update
+    
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetScore()
+    {
+        return 10;
+    }
+}
